1dc7c2efe09de4cdbb7f50d0ae1ce34ebb8fd6da
Peanutccino <Peanutccino@vs>
2024-08-08 14:47:05 +0200

    - Now adds new hotkeys automatically when loaded up - This was prevented before, when a hotkey.json was already present - Improved error handling - Fixed hotkey.json location not loading properly before

--- RandomVideoPlayerV3/Functions/HotkeySetting.cs
+++ RandomVideoPlayerV3/Functions/HotkeySetting.cs
@@ -14,7 +14,7 @@ namespace RandomVideoPlayer.Functions
     }
     public static class HotkeyManager
     {
-        private static readonly string SettingsFilePath = "hotkeys.json";
+        private static readonly string SettingsFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hotkeys.json");
 
         public static HotkeySettings GetDefaultHotkeySettings()
         {
@@ -38,24 +38,76 @@ namespace RandomVideoPlayer.Functions
                     new HotkeySetting { Action = "VolumeDecrease", Key = Keys.Down, Modifiers = Keys.None },
                     new HotkeySetting { Action = "ToggleExclusiveFullscreen", Key = Keys.F11, Modifiers = Keys.None },
                     new HotkeySetting { Action = "SeekForward", Key = Keys.Right, Modifiers = Keys.Alt },
-                    new HotkeySetting { Action = "SeekBackward", Key = Keys.Left, Modifiers = Keys.Alt }
+                    new HotkeySetting { Action = "SeekBackward", Key = Keys.Left, Modifiers = Keys.Alt },
+                    new HotkeySetting { Action = "ZoomIn", Key = Keys.NumPad9, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "ZoomOut", Key = Keys.NumPad7, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "PanLeft", Key = Keys.NumPad4, Modifiers = Keys.Control },
+                    new HotkeySetting { Action = "PanRight", Key = Keys.NumPad6, Modifiers = Keys.Control },
+                    new HotkeySetting { Action = "PanUp", Key = Keys.NumPad8, Modifiers = Keys.Control },
+                    new HotkeySetting { Action = "PanDown", Key = Keys.NumPad2, Modifiers = Keys.Control },
+                    new HotkeySetting { Action = "FitHorizontal", Key = Keys.NumPad3, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "FitVertical", Key = Keys.NumPad1, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "ScaleWidthUp", Key = Keys.NumPad6, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "ScaleWidthDown", Key = Keys.NumPad4, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "ScaleHeightUp", Key = Keys.NumPad8, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "ScaleHeightDown", Key = Keys.NumPad2, Modifiers = Keys.None },
+                    new HotkeySetting { Action = "RotateClockwise", Key = Keys.NumPad3, Modifiers = Keys.Alt },
+                    new HotkeySetting { Action = "Rotate180", Key = Keys.NumPad2, Modifiers = Keys.Alt },
+                    new HotkeySetting { Action = "RotateCounterClockwise", Key = Keys.NumPad1, Modifiers = Keys.Alt },
+                    new HotkeySetting { Action = "ResetVideoManipulation", Key = Keys.NumPad5, Modifiers = Keys.None }
                 }
             };
         }
         public static HotkeySettings LoadHotkeySettings()
         {
-            if (File.Exists(SettingsFilePath))
+            var defaultSettings = GetDefaultHotkeySettings();
+
+            try
             {
-                var json = File.ReadAllText(SettingsFilePath);
-                return JsonSerializer.Deserialize<HotkeySettings>(json);
+                if (File.Exists(SettingsFilePath))
+                {
+                    var json = File.ReadAllText(SettingsFilePath);
+                    var userSettings = JsonSerializer.Deserialize<HotkeySettings>(json);
+
+                    foreach (var defaultHotkey in defaultSettings.Hotkeys)
+                    {
+                        if (!userSettings.Hotkeys.Any(h => h.Action == defaultHotkey.Action))
+                        {
+                            userSettings.Hotkeys.Add(defaultHotkey);
+                        }
+                    }
+
+                    SaveHotkeySettings(userSettings);
+
+                    return userSettings;
+                }
+                else
+                {
+                    SaveHotkeySettings(defaultSettings);
+                    return defaultSettings;
+                }
             }
-            return GetDefaultHotkeySettings();
+            catch (Exception ex)
+            {
+                Error.Log(ex, "Failed to load hotkeySettings");
+                return defaultSettings;
+            }
+
+
         }
 
         public static void SaveHotkeySettings(HotkeySettings settings)
         {
-            var json = JsonSerializer.Serialize(settings);
-            File.WriteAllText(SettingsFilePath, json);
+            try
+            {
+                var json = JsonSerializer.Serialize(settings);
+                File.WriteAllText(SettingsFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                Error.Log(ex, "Couldn't save hotkeys.json");
+            }
+
         }
     }
 }
