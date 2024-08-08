393ca65286fcf94b881e07c64df51e4c70621676
Peanutccino <Peanutccino@vs>
2024-08-08 14:18:45 +0200

    - Added Error handler for more expressive exceptions

---
+++ RandomVideoPlayerV3/Functions/Error.cs
@@ -0,0 +1,43 @@
+﻿
+namespace RandomVideoPlayer.Functions
+{
+    public static class Error
+    {
+        private static readonly string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "error.log");
+
+        public static void Log(string customMessage)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine("Date: " + DateTime.Now.ToString());
+                    writer.WriteLine("Custom Message: " + customMessage);
+                    writer.WriteLine("---------------------------------------------------");
+                }
+            }
+            catch (Exception loggingEx)
+            {
+                MessageBox.Show("An error occurred while logging: " + loggingEx.Message);
+            }
+        }
+        public static void Log(Exception ex, string customMessage)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine("Date: " + DateTime.Now.ToString());
+                    writer.WriteLine("Message: " + ex.Message);
+                    writer.WriteLine("StackTrace: " + ex.StackTrace);
+                    writer.WriteLine("Custom Message: " + customMessage);
+                    writer.WriteLine("---------------------------------------------------");
+                }
+            }
+            catch (Exception loggingEx)
+            {
+                MessageBox.Show("An error occurred while logging: " + loggingEx.Message);
+            }
+        }
+    }
+}
